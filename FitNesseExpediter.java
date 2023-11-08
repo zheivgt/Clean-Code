b5b923d2c93b54c965164c731075b3d947615934
zheivgt <133962917+zheivgt@users.noreply.github.com>
2023-11-08 09:13:17 +0700

    Sebelum di update

---
+++ FitNesseExpediter.java
@@ -0,0 +1,13 @@
+package cleancode;
+public class FitNesseExpediter implements ResponseSender {
+    private     Socket               socket;
+    private     InputStream          input;
+    private     OutputStream         output;
+    private     Request              request;
+    private     Response             response;
+    private     FitNesseContext      context;
+    protected   long                 requestParsingTimeLimit;
+    private     long                 requestProgress;
+    private     long                 requestParsingDeadline;
+    private     boolean              hasError;
+}
