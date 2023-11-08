9d2261e69a9e5c017f93db86c8e78df316ecd243
zheivgt <133962917+zheivgt@users.noreply.github.com>
2023-11-08 08:17:42 +0700

    Sebelum di update

---
+++ Minesweeper.java
@@ -0,0 +1,9 @@
+public class Minesweeper{
+    public List<int[]> getThem() { 
+        List<int[]> list1 = new ArrayList<int[]>(); 
+        for (int[] x : theList) 
+        if (x[0] == 4) 
+        list1.add(x); 
+        return list1; 
+    }
+}
