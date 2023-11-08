fceea494d5191c3df29c6e331b90ac00885c032e
zheivgt <133962917+zheivgt@users.noreply.github.com>
2023-11-08 08:58:17 +0700

    Legal Comments (Good Comments)

---
+++ GoodComments.java
@@ -0,0 +1,5 @@
+public class GoodComments{
+    // Copyright (C) 2011 by Osoco. All rights reserved. 
+    // Released under the terms of the GNU General Public License 
+    // version 2 or later. 
+}
