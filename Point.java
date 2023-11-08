c153f98d216dcface2c4858ea50817d0daef2576
zheivgt <133962917+zheivgt@users.noreply.github.com>
2023-11-08 08:46:39 +0700

    Sesudah di update

--- Point.java
+++ Point.java
@@ -1,4 +1,8 @@
-public class Point { 
-    public double x; 
-    public double y; 
-} 
+public interface Point { 
+    double getX(); 
+    double getY(); 
+    void setCartesian(double x, double y); 
+    double getR(); 
+    double getTheta(); 
+    void setPolar(double r, double theta); 
+}
