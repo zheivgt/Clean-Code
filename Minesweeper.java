58bc90b5f145f89607300d777a7a8881d4743ae2
zheivgt <133962917+zheivgt@users.noreply.github.com>
2023-11-08 08:21:10 +0700

    Setelah di update

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Minesweeper{
-    public List<int[]> getThem() { 
-        List<int[]> list1 = new ArrayList<int[]>(); 
-        for (int[] x : theList) 
-        if (x[0] == 4) 
-        list1.add(x); 
-        return list1; 
+    public final static int STATUS_VALUE = 0; 
+    public final static int FLAGGED = 4; 
+    public List<int[]> getFlaggedCells() { 
+        List<int[]> flaggedCells = new ArrayList<int[]>(); 
+        Iterable<int[]> gameBoard = null;
+        for (int[] cell : gameBoard) 
+        if (cell[STATUS_VALUE] == FLAGGED) 
+        flaggedCells.add(cell); 
+        return flaggedCells; 
     }
 }
