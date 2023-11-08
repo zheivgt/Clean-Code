385835e611b276a72f314dee3ba6cfb841abae99
zheivgt <133962917+zheivgt@users.noreply.github.com>
2023-11-08 09:03:05 +0700

    Clarification (Good Comments)

--- GoodComments.java
+++ GoodComments.java
@@ -1,9 +1,6 @@
 import java.util.regex.Pattern;
 
 public class GoodComments{
-// Returns an instance of the Responder being tested. 
-protected abstract Responder responderInstance(); 
-// renaming the function: responderBeingTested 
-// format matched kk:mm:ss EEE, MMM dd, yyyy 
-Pattern timeMatcher = Pattern.compile("\\d*:\\d*:\\d* \\w*, \\w* \\d*, \\d*"); 
+    assertTrue(a.compareTo(b) == -1); // a < b 
+    assertTrue(b.compareTo(a) == 1); // b > a
 }
