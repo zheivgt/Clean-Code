c50388745dc2faf7604b77ef6dd92ac209c3f6ec
zheivgt <133962917+zheivgt@users.noreply.github.com>
2023-11-08 08:59:10 +0700

    Informative Comments (Good Comments)

--- GoodComments.java
+++ GoodComments.java
@@ -1,5 +1,9 @@
+import java.util.regex.Pattern;
+
 public class GoodComments{
-    // Copyright (C) 2011 by Osoco. All rights reserved. 
-    // Released under the terms of the GNU General Public License 
-    // version 2 or later. 
+// Returns an instance of the Responder being tested. 
+protected abstract Responder responderInstance(); 
+// renaming the function: responderBeingTested 
+// format matched kk:mm:ss EEE, MMM dd, yyyy 
+Pattern timeMatcher = Pattern.compile("\\d*:\\d*:\\d* \\w*, \\w* \\d*, \\d*"); 
 }
