5e74311778538117a9bbab1f4289819c4f5aa76d
zheivgt <133962917+zheivgt@users.noreply.github.com>
2023-11-11 08:19:23 +0700

    Sebelum di update

---
+++ Tugas Defensive Programming/ListOfNumbers2.java
@@ -0,0 +1,25 @@
+import java.io.*; 
+import java.util.Vector; 
+public class ListOfNumbers2 { 
+    private Vector victor; 
+    private static final int size = 10; 
+    public ListOfNumbers2() { 
+        victor = new Vector(size); 
+        for (int i = 0; i < size; i++) 
+        victor.addElement(new Integer(i)); 
+        this.readList("infile.txt"); 
+        this.writeList(); 
+    } 
+    public void readList(String fileName) { 
+        String line = null; 
+        RandomAccessFile raf = new RandomAccessFile(fileName, "r"); 
+        while ((line = raf.readLine()) != null) { 
+            Integer i = new Integer(Integer.parseInt(line)); 
+            System.out.println(i); 
+            victor.addElement(i); 
+        } 
+    } 
+    //tambahkan exception jika file tidak ditemukan 
+    //tambahkan exception untuk output error 
+ } 
+ public void writeList() {
